1948ff08469fd302dbe9ab247a01467d4b8ef4ad
Shunsuke0205 <hirata-shunsuke1125@g.ecc.u-tokyo.ac.jp>
2025-02-22 22:51:27 +0900

    impl: footer

---
+++ src/app/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="py-3 px-4 border-t">
+      <small>@2025 Shunsuke HIRATA</small>
+    </footer>
+  )
+};
+
+export default Footer;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
       >
         <Header />
         {children}
-        <footer>Footer</footer>
+        <Footer />
       </body>
     </html>
   );
