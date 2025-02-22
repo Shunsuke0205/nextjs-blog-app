306a31050d181aa8948988bda6992bb9d6117488
Shunsuke0205 <hirata-shunsuke1125@g.ecc.u-tokyo.ac.jp>
2025-02-22 23:18:07 +0900

    impl: text below the photo

--- src/app/components/ArticleList.tsx
+++ src/app/components/ArticleList.tsx
@@ -14,6 +14,15 @@ const ArticleList = () => {
             height={720}
           />
         </Link>
+        <div>
+          <Link href="#">Technology</Link>
+          <Link href="#">Learning Next.js</Link>
+          <p>By Shunsuke, published on 2025/02/22</p>
+          <Link href="#">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi a expedita magni ullam rerum, deleniti tempore eos soluta. Sit, dolor optio quos quam maiores praesentium! Tempore tenetur obcaecati rerum voluptates.
+          </Link>
+          <Link href="#">read more</Link>
+        </div>
       </article>
     </div>
   )
