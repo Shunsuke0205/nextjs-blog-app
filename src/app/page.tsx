3aa3455d197d6cfc284d376d423f9aa041ef3955
Shunsuke0205 <hirata-shunsuke1125@g.ecc.u-tokyo.ac.jp>
2025-02-22 22:52:53 +0900

    impl: article list component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
 import Image from "next/image";
+import ArticleList from "./components/ArticleList";
 
 export default function Home() {
   return (
-    <div>記事投稿</div>
+    <div className="md:flex">
+      <section className="w-full md:w-2/3 flex flex-col items-center px-3">
+        <ArticleList />
+      </section>
+
+      
+    </div>
   );
 }
