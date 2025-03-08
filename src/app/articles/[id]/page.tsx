8ea10ffcc51ff86c392d4aacf76636e62354544e
Shunsuke0205 <hirata-shunsuke1125@g.ecc.u-tokyo.ac.jp>
2025-03-08 17:58:04 +0900

    impl: get an article in JSON

--- src/app/articles/[id]/page.tsx
+++ src/app/articles/[id]/page.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import Image from 'next/image'
+import { getArticleDetail } from '@/blogAPI';
 
-const Article = ({ params }: {params: {id: string} }) => {
-  console.log(params.id);
+const Article = async ({ params }: {params: {id: string} }) => {
+
+  const articleDetail = await getArticleDetail(params.id);
+  console.log(articleDetail);
   return (
     <div className="max-w-3xl mx-auto p-5">
       <Image
