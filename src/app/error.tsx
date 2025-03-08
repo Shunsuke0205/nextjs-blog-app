2a9a5bd0bfd39b0b133f37f68cba8218659dce68
Shunsuke0205 <hirata-shunsuke1125@g.ecc.u-tokyo.ac.jp>
2025-03-08 13:38:48 +0900

    impl: css

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -2,9 +2,12 @@
 
 import React from 'react'
 
-const Error = () => {
+const Error = ({ reset }: { reset: () => void }) => {
   return (
-    <div>Error</div>
+    <div className="bg-red-100 border-l-4 border-red-500 text-red-700 mt-4 rounded shadow-md mx-auto p-2">
+      <h3 className="font-bold mb-2">An error occurred.</h3>
+      <button onClick={() => reset()} className="bg-red-600 text-white px-4 py-2 rounded hover:bg-blue-500 transition duration-700">reload the page</button>
+    </div>
   )
 }
 
