718ee81cb3d057d041c57cd1a71be2a2dd8c8a92
Shunsuke0205 <hirata-shunsuke1125@g.ecc.u-tokyo.ac.jp>
2025-03-08 17:57:10 +0900

    impl: get an article

--- src/blogAPI.ts
+++ src/blogAPI.ts
@@ -1,3 +1,4 @@
+import { notFound } from "next/navigation";
 import { Article } from "./types";
 
 export const getALLArticles = async (): Promise<Article[]> => {
@@ -10,8 +11,27 @@ export const getALLArticles = async (): Promise<Article[]> => {
     throw new Error("Failed to fetch articles.");
   }
 
-  await new Promise((resolve) => setTimeout(resolve, 2000 /* milliseconds */));
+  await new Promise((resolve) => setTimeout(resolve, 1000 /* milliseconds */));
 
   const articles = await response.json();
   return articles;
+};
+
+export const getArticleDetail = async (id: string): Promise<Article> => {
+  const response = await fetch(`http://localhost:8000/posts/${id}`, {
+    next: { revalidate: 60 /* seconds */ }, // ISR
+  });
+
+  if (response.status === 404) {
+    notFound();
+  }
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch the article.");
+  }
+
+  await new Promise((resolve) => setTimeout(resolve, 2000 /* milliseconds */));
+
+  const article = await response.json();
+  return article;
 };
