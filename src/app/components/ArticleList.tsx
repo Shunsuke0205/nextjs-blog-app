b63f8288e4bdfe4ebc63929ddb2e8b705ddd5640
Shunsuke0205 <hirata-shunsuke1125@g.ecc.u-tokyo.ac.jp>
2025-03-09 09:56:01 +0900

    refactor: create ArticleCard component

--- src/app/components/ArticleList.tsx
+++ src/app/components/ArticleList.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
-import Link from 'next/link'
-import Image from 'next/image'
 import { Article } from '@/types'
+import ArticleCard from './ArticleCard';
 
 type ArticleListProps = {
   articles: Article[];
@@ -10,27 +9,9 @@ type ArticleListProps = {
 const ArticleList = ({ articles }: ArticleListProps) => {
   return (
     <div>
-      {articles.map((article) => 
-        <article className="shadow my-4" key={article.id}>
-          <Link href={`articles/${article.id}`} className="hover:opacity-75">
-            <Image
-              src={`https://picsum.photos/id/${9 + parseInt(article.id)}/1280/300`}
-              alt="random image"
-              width={1280}
-              height={300}
-            />
-          </Link>
-          <div className='bg-white flex flex-col justify-start p-6'>
-            <Link href={`articles/${article.id}`} className="text-blue-700 pb-4 font-bold">Technology</Link>
-            <Link href={`articles/${article.id}`} className="text-slate-900 text-3xl font-bold hover:text-gray-700  pb-4">{article.title}</Link>
-            <p className='text-sm pb-3 text-slate-900'>Publish on {article.date}</p>
-            <Link href={`articles/${article.id}`} className='text-slate-900 pb-6'>
-              {article.content.length > 500 ? article.content.substring(0, 500) + "......" : article.content}
-            </Link>
-            <Link href={`articles/${article.id}`} className='text-pink-900 hover:text-black'>read more</Link>
-          </div>
-        </article>
-      )}
+      {articles.map((article) => (
+        <ArticleCard key={article.id} article={article} />
+      ))}
     </div>
   )
 }
