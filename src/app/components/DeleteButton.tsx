7b6a69b65eb821933cdd4e95cc105b45af4ab04b
Shunsuke0205 <hirata-shunsuke1125@g.ecc.u-tokyo.ac.jp>
2025-03-15 22:36:02 +0900

    impl: DeleteButton component

---
+++ src/app/components/DeleteButton.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { deleteArticle } from "@/blogAPI";
+import { useRouter } from "next/navigation";
+import React from "react"
+
+type DeleteButtonProps = {
+  id: string;
+};
+
+const DeleteButton = ({ id }: DeleteButtonProps) => {
+  const router = useRouter();
+
+  const handleDelete = async () => {
+    await deleteArticle(id);
+
+    router.push("/");
+    router.refresh();
+  }
+  return (
+    <div
+      className="bg-red-500 hover:bg-red-700 rounded-md py-2 px-5 inline cursor-pointer"
+      onClick={handleDelete}
+    >
+      Delete
+    </div>
+  )
+}
+
+export default DeleteButton
